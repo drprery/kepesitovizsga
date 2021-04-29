63add43aea65a75f61caa6b2f5919b18202cfaa4
drprery <kpusztai777@gmail.com>
2021-04-29 10:50:54 +0200

    AddDigits

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -0,0 +1,21 @@
+package hu.nive.ujratervezes.kepesitovizsga.addigits;
+
+public class AddDigits {
+
+
+    public int addDigits(String input) {
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sum += Character.getNumericValue(c);
+            }
+        }
+
+        return sum;
+    }
+}
